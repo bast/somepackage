604cb4c35c2e83a11971338c64c814880b5b307e
Radovan Bast <bast@users.noreply.github.com>
2018-01-04 23:37:50 +0100

    describe __all__

--- somepackage/__init__.py
+++ somepackage/__init__.py
@@ -2,6 +2,8 @@ from .version import version_info, __version__
 from .module1 import some_function
 from .module2 import another_function
 
+# if somebody does "from somepackage import *", this is what they will
+# be able to access:
 __all__ = [
     'version_info',
     'some_function',
