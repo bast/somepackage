0171ac9a6627614216fabf355d39ab607a48d046
Radovan Bast <bast@users.noreply.github.com>
2018-12-09 15:40:27 +0100

    refactor version.py: keep it simple

--- somepackage/__init__.py
+++ somepackage/__init__.py
@@ -1,11 +1,10 @@
-from .version import version_info, __version__
+from .version import __version__
 from .module1 import some_function
 from .module2 import another_function
 
 # if somebody does "from somepackage import *", this is what they will
 # be able to access:
 __all__ = [
-    'version_info',
     'some_function',
     'another_function',
 ]
