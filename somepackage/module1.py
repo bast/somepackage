e6041954c3a41cec6504dce1afaf1407e5cd6852
Radovan Bast <bast@users.noreply.github.com>
2018-12-09 15:57:27 +0100

    more explicit example for the internal function

--- somepackage/module1.py
+++ somepackage/module1.py
@@ -6,7 +6,7 @@ def some_function():
     Describe here what this function does,
     its input parameters, and what it returns.
     """
-    t = _internal_function('hey ho -')
+    t = _shout('hey ho -')
     return another_function(t, t)
 
 
@@ -14,14 +14,19 @@ def test_some_function():
     assert some_function() == 'HEY HO -HEY HO -'
 
 
-# the underscore signals that this function is not to be used outside
-def _internal_function(parameter):
+# the underscore signals that this function is not to be used outside this
+# module
+def _shout(text):
     """
     Describe here what this function does,
     its input parameters, and what it returns.
+
+    In this example the function converts the input string
+    to uppercase.
     """
-    return parameter.upper()
+    result = text.upper()
+    return result
 
 
-def test_internal_function():
-    assert _internal_function('shouting') == 'SHOUTING'
+def test_shout():
+    assert _shout('have a nice day') == 'HAVE A NICE DAY'
